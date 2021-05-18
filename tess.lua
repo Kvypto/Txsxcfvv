ecdb7f12f81d28652c1ae70efa0f99003c2987e2
Kvypto <81843251+Kvypto@users.noreply.github.com>
2021-05-17 23:12:13 -0400

    Update tess.lua

--- tess.lua
+++ tess.lua
@@ -1,14 +1,14 @@
 for i,v in next, game:GetService("Players").LocalPlayer.Character:GetDescendants() do
 if v:IsA("BasePart") and v.Name ~="HumanoidRootPart" then 
 game:GetService("RunService").Heartbeat:connect(function()
-v.Velocity = Vector3.new(45,54,27)
+v.Velocity = Vector3.new(15,24,17)
 end)
 end
 end
 
 local ME = game:GetService("Players").LocalPlayer
 local HB = game:GetService("RunService").Heartbeat
-local vel = Vector3.new(25, 25, 20)
+local vel = Vector3.new(15, 15, 0)
 for _, v in ipairs(ME.Character:GetDescendants()) do
     if v:IsA("BasePart") and v.Name == "Handle" then
         HB:Connect(function()
