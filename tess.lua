04d198884b6d2c36c6e4a73a8a0924eb8e16dad1
Kvypto <81843251+Kvypto@users.noreply.github.com>
2021-05-18 00:21:55 -0400

    Update tess.lua

--- tess.lua
+++ tess.lua
@@ -8,7 +8,7 @@ end
 
 local ME = game:GetService("Players").LocalPlayer
 local HB = game:GetService("RunService").Heartbeat
-local vel = Vector3.new(55, 40, 60)
+local vel = Vector3.new(25, 30, 10)
 for _, v in ipairs(ME.Character:GetDescendants()) do
     if v:IsA("BasePart") and v.Name == "Handle" then
         HB:Connect(function()
