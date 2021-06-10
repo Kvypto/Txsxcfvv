5a66cd0c5c74ae7da60e2636ea5a66adba102b91
Kvypto <81843251+Kvypto@users.noreply.github.com>
2021-06-09 20:49:05 -0400

    Update fix.lua

--- fix.lua
+++ fix.lua
@@ -8,3 +8,14 @@ for _, v in ipairs(ME.Character:GetDescendants()) do
         end)
     end
 end
+
+local ME = game:GetService("Players").LocalPlayer
+local HB = game:GetService("RunService").Heartbeat
+local vel = Vector3.new(-17, 0, -17)
+for _, v in ipairs(ME.Character:GetDescendants()) do
+    if v:IsA("BasePart") and v.Name == "HumanoidRootPart" then
+        HB:Connect(function()
+            v.Velocity = vel
+        end)
+    end
+end
