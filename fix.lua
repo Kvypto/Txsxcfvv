f7951e42a665eb6aa5b978f2a2e14c0ef3c9a6a7
Kvypto <81843251+Kvypto@users.noreply.github.com>
2021-06-25 02:06:25 -0400

    Update fix.lua

--- fix.lua
+++ fix.lua
@@ -1,7 +1,7 @@
 local ME = game:GetService("Players").LocalPlayer
 local HB = game:GetService("RunService").Heartbeat
 local vel = Vector3.new(7.08782e-06, 6.12542e-08, 8.28769e-06)
-local rotvel = Vector3.new(2, -5, 2)
+local rotvel = Vector3.new(16, -8, 9)
 for _, v in ipairs(ME.Character:GetDescendants()) do
     if v:IsA("BasePart") and v.Name == "Handle" then
         HB:Connect(function()
